aeef42443f010025cc580aa769633c415de7ccb2
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2023-03-23 18:04:27 +0900

    feat : quotes function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const QUOTES = ".quotes";
+
 function getTime() {
   const time = document.querySelector(".time");
 
@@ -15,3 +17,54 @@ getTime();
 setInterval(() => {
   getTime();
 }, 1000);
+
+function getQuotes() {
+  const quotesMsg = document.querySelector(".quotesMsg");
+  let savedQuotes = localStorage.getItem(QUOTES);
+
+  if (!savedQuotes) {
+    localStorage.setItem(
+      QUOTES,
+      JSON.stringify([
+        "열심히 살지맙시다.",
+        "그래도 열심히 살아야지.",
+        "열심히 살면 뭐해~",
+        "열심히 살면 반드시 빛이 온다.",
+      ])
+    );
+
+    savedQuotes = localStorage.getItem(QUOTES);
+  }
+  let quotesArray = JSON.parse(savedQuotes);
+
+  quotesMsg.innerText =
+    quotesArray[Math.floor(Math.random() * quotesArray.length)];
+}
+
+getQuotes();
+
+function onClickAdd() {
+  const newQuotes = document.querySelector(".newQuotes");
+
+  newQuotes.style.display = "inline-block";
+}
+
+function onClickRegist() {
+  const quotesMsg = document.querySelector(".quotesMsg");
+  const newQuotesInput = document.querySelector(".newQuotesInput");
+  const newQuotes = document.querySelector(".newQuotes");
+
+  if (!newQuotesInput.value) {
+    return;
+  }
+
+  let savedQuotes = localStorage.getItem(QUOTES);
+
+  let quotesArray = JSON.parse(savedQuotes);
+  quotesArray.push(newQuotesInput.value);
+
+  localStorage.setItem(QUOTES, JSON.stringify(quotesArray));
+
+  quotesMsg.innerHTML = `<div>${newQuotesInput.value}</div>`;
+  newQuotes.style.display = "none";
+}
